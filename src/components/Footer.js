492e6a3433e35d8424338aba6740dfb5b0b6d9fb
sikandarkr <royalsikandar25@gmail.com>
2024-09-09 13:47:54 +0530

    footer modification

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import '../styles/Footer.css';
 
 
 const Footer = () => {
+  const [isVisible, setIsVisible] = useState(true);
+
+  const handleRemoveFooter = () => {
+    setIsVisible(false);
+  };
+
   return (
-    <footer className="footer">
-      <p>&copy; <strong>Declaration:</strong> I hereby declare that all the information mentioned above is true and I bear the responsibility for the above-mentioned particulars is to the best of my knowledge and belief.</p>
-    </footer>
+    isVisible && (
+      <footer className="footer">
+        <button className="close-btn" onClick={handleRemoveFooter}>
+         <p>X</p>
+        </button>
+        <p>&copy; <strong>Declaration:</strong> I hereby declare that all the information mentioned above is true and I bear the responsibility for the above-mentioned particulars to the best of my knowledge and belief.</p>
+      </footer>
+    )
   );
 };
 
